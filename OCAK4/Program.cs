c4426fc1e4ee6403e519d4d22016bee1fb1b6731
betularass <betularas16@gmail.com>
2025-03-15 18:11:14 +0300

    OCAK4

---
+++ OCAK4/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+
+// 1.SORU 
+
+
+// For döngüsü, programlama dillerinde temel olarak bir kod blokunu belirli bir sayıda ve üst üste çalıştırmak için kullanılan bir döngüdür.
+//while döngüsü: Koşula bağlı olarak çalışır ve koşul sağlandığı sürece döngü devam eder. Genellikle tekrar sayısı önceden bilinmeyen durumlarda tercih edilir.
+
+
+
+    // 2.SORU
+
+/*
+for (int i = 1; i <= 10; i++)
+{
+    Console.WriteLine(i);
+}
+*/
+
+
+     // 3.SORU
+
+/*
+
+    {
+        Console.Write("Bir sayı girin: ");
+        int sayi = int.Parse(Console.ReadLine());
+
+        int toplam = 0, i = 1;
+        while (i <= sayi)
+        {
+            toplam += i;
+            i++;
+        }
+
+        Console.WriteLine("Toplam: " + toplam);
+    }
+
+*/
+
